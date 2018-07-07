9e72842cc52635e5d3c945666e179a474ee1312c
Biryukov-Nikolay <jackson_klyazma@mail.ru>
2018-07-07 20:23:34 +0300

    Пиксель перфект

--- js/js.js
+++ js/js.js
@@ -1,34 +1,61 @@
-var mapbtn = document.querySelector(".map");
-    mapframe = document.querySelector(".map-window");
-    closemodal = document.querySelector(".form-close");
-    formbtn  = document.querySelector(".map-btn");
-    formframe = document.querySelector(".form-answer");
-    buy  = document.querySelectorAll(".buy-btn");
-    cartframe = document.querySelector(".cart-add");
-
-    mapbtn.addEventListener("click", function(evt) {
-        evt.preventDefault(); 
-        mapframe.classList.add("modal-show");
-    });
-    closemodal.addEventListener("click", function(evt) {
-        evt.preventDefault();      
-        mapframe.classList.remove("modal-show");
-    });
-
-        formbtn.addEventListener("click", function(evt) {
-        evt.preventDefault(); 
-        formframe.classList.add("modal-show");
-    });
-    closemodal.addEventListener("click", function(evt) {
-        evt.preventDefault();      
-        formframe.classList.remove("modal-show");
-    });
-
-        buy.addEventListener("click", function(evt) {
-        evt.preventDefault(); 
-        cartframe.classList.add("modal-show");
-    });
-    closemodal.addEventListener("click", function(evt) {
-        evt.preventDefault();      
-        cartframe.classList.remove("modal-show");
-    });
+// Важность директивы var https://bit.ly/2NvPeQ7
+ 
+ var mapbtn = document.querySelector('.map');
+ var mapframe = document.querySelector('.map-window');
+ var closemodal = document.querySelectorAll('.form-close'); // Тут у нас несколько элементов на странице, используем querySelectorAll и получим массив элементов
+ var formbtn = document.querySelector('.map-btn');
+ var formframe = document.querySelector('.form-answer');
+ var buy = document.querySelectorAll('.buy-btn'); // Тоже может быть несколько элементов, используем querySelectorAll
+ var cartframe = document.querySelector('.cart-add');
+ 
+ // Элементов может не быть на странице, поэтому нужно проверить их наличие
+ 
+ if (mapbtn) {
+   mapbtn.addEventListener('click', function(evt) {
+     evt.preventDefault();
+     closeAllModals();
+     mapframe.classList.add('modal-show');
+   });
+ }
+ 
+ if (formbtn) {
+   formbtn.addEventListener('click', function(evt) {
+     evt.preventDefault();
+     closeAllModals();
+     formframe.classList.add('modal-show');
+   });
+ }
+ 
+ // Массивы элементов переберем с помощью цикла for, и каждому навесим обработчик клика
+ 
+ for (var i = 0; i < buy.length; i++) {
+   buy[i].addEventListener('click', function(evt) {
+     evt.preventDefault();
+     closeAllModals(); // Закрываем все активные окна
+     cartframe.classList.add('modal-show'); // Показываем нужное окно
+   });
+ }
+ 
+ for (var i = 0; i < closemodal.length; i++) {
+   closemodal[i].addEventListener('click', function(evt) {
+     evt.preventDefault();
+     closeAllModals();
+   });
+ }
+ 
+ // Признаком хорошего интерфейса, является не более активного одного модального окна на странице
+ // Создадим функцию, которая закрывает все модальные окна
+ 
+ function closeAllModals() {
+   if (cartframe) {
+     cartframe.classList.remove('modal-show');
+   }
+ 
+   if (formframe) {
+     formframe.classList.remove('modal-show');
+   }
+ 
+   if (mapframe) {
+     mapframe.classList.remove('modal-show');
+   }
+ }
