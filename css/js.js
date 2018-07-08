c7e9d08008bf96ba6a80dead5c3fae7b2d8fcbe1
Biryukov-Nikolay <jackson_klyazma@mail.ru>
2018-07-08 13:49:03 +0300

    Доводка

--- css/js.js
+++
@@ -1,8 +0,0 @@
-var mapbtn = document.querySelector(".map");
-    mapframe = document.querySelector(".map-window");
-    closemodal = document.querySelector(".form-close");
-
-    mapbtn.addEventListener("click", function(evt) {
-        evt.preventDefault(); 
-        mapframe.classList.add("modal-show");
-        });
--- js/js.js
+++ js/js.js
@@ -1,14 +1,14 @@
-// Важность директивы var https://bit.ly/2NvPeQ7
+
 
 var mapbtn = document.querySelector('.map');
 var mapframe = document.querySelector('.map-window');
-var closemodal = document.querySelectorAll('.form-close'); // Тут у нас несколько элементов на странице, используем querySelectorAll и получим массив элементов
+var closemodal = document.querySelectorAll('.form-close'); 
 var formbtn = document.querySelector('.map-btn');
 var formframe = document.querySelector('.form-answer');
-var buy = document.querySelectorAll('.buy-btn'); // Тоже может быть несколько элементов, используем querySelectorAll
+var buy = document.querySelectorAll('.buy-btn'); 
 var cartframe = document.querySelector('.cart-add');
 
-// Элементов может не быть на странице, поэтому нужно проверить их наличие
+
 
 if (mapbtn) {
   mapbtn.addEventListener('click', function(evt) {
@@ -26,13 +26,12 @@ if (formbtn) {
   });
 }
 
-// Массивы элементов переберем с помощью цикла for, и каждому навесим обработчик клика
 
 for (var i = 0; i < buy.length; i++) {
   buy[i].addEventListener('click', function(evt) {
     evt.preventDefault();
-    closeAllModals(); // Закрываем все активные окна
-    cartframe.classList.add('modal-show'); // Показываем нужное окно
+    closeAllModals(); 
+    cartframe.classList.add('modal-show'); 
   });
 }
 
@@ -43,8 +42,6 @@ for (var i = 0; i < closemodal.length; i++) {
   });
 }
 
-// Признаком хорошего интерфейса, является не более активного одного модального окна на странице
-// Создадим функцию, которая закрывает все модальные окна
 
 function closeAllModals() {
   if (cartframe) {
